0c5ee5b2be0557cfe69675eab53a7af537fb9ad1
urayukitaka <99265078+urayukitaka@users.noreply.github.com>
2023-04-29 14:34:59 +0900

    utils.pyの新規作成
    関数を1つ追加。
        データを保存する用のディレクトリを作り、ディレクトリパスを返す。

---
+++ scripts/utils.py
@@ -0,0 +1,29 @@
+'''
+utils
+
+各種共通関数
+'''
+################################
+# Libraries
+################################
+import os
+from datetime import datetime as dt
+
+################################
+# Functions
+################################
+
+# Prepare directory
+def prepare_savedir(ROOT:str, name:str)->str:
+    '''
+    Args
+        ROOT (str) : Directory for save
+        name (str) : Save directory name
+    '''
+
+    # define directory
+    savedir = os.path.join(ROOT, name)
+    # make direcotry
+    os.makedirs(savedir, exist_ok=True)
+
+    return savedir
